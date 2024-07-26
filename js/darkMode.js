3d1d5495c989b0aebaf1496d2e6cf90c070d8b0d
Aman Reddy Pundru <reddyaman77.ar@gmail.com>
2024-07-26 18:46:47 +0530

    Delete js/darkMode.js

--- js/darkMode.js
+++
@@ -1,8 +0,0 @@
-// Immediately set the theme based on user preference or system setting
-(function() {
-    const userPreference = localStorage.getItem('theme') || 'system';
-    const mode = (userPreference === 'light') ? 'light' :
-        (userPreference === 'dark') ? 'dark' :
-        (matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark');
-    document.documentElement.dataset.appliedMode = mode;
-})();
